1d1d1f226c4bee9eedab7966465625e3e80566ac
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-08-30 17:03:38 -0300

    Login page style and page transition

--- source/rateme/src/pages/login/login.ts
+++ source/rateme/src/pages/login/login.ts
@@ -8,11 +8,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
+  email: string;
+  password: string;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+
+  //Function to open another page
+  //When we use PUSH method, we will see the Back Button Page on the next page
+  //If we do not want to see, we use setRoot method
+  registerPage(){
+    //Lazy loading on string
+    this.navCtrl.push("RegisterPage");
   }
 
 }
