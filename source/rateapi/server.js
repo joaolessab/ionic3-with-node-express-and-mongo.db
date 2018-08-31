9f97818d52e9c3cd9b737cee4c0606e04586984e
João Vitor Lessa <joaovitorlessa@gmail.com>
2018-08-31 17:34:32 -0300

    Node Server
    Node Server

---
+++ source/rateapi/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/ratepp');
+
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'thisisasecretkey',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
